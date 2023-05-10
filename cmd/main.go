941d270bf551004025ecb3d05242f282078b0300
Serhii Bohoslavskyi <serhii.bohoslavskyi@gmail.com>
2023-05-10 22:47:25 +0300

    Add empty handlers to endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	"github.com/bohoslavskyi/go-todo-app"
+	"github.com/bohoslavskyi/go-todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	server := new(todo.Server)
-	if err := server.Run("8000"); err != nil {
+	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
