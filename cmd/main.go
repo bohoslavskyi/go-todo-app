f7723fdecc789302391dadd5a8f4235aa139936f
Serhii Bohoslavskyi <serhii.bohoslavskyi@gmail.com>
2023-05-11 23:41:04 +0300

    Use Viper module for configuration

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,27 @@ import (
 	"github.com/bohoslavskyi/go-todo-app/pkg/handler"
 	"github.com/bohoslavskyi/go-todo-app/pkg/repository"
 	"github.com/bohoslavskyi/go-todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repositories := repository.NewRepository()
 	services := service.NewService(repositories)
 	handlers := handler.NewHandler(services)
 
 	server := new(todo.Server)
-	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	return viper.ReadInConfig()
 }
